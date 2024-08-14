0caaf2b2c7916b4177d22218ec9e313014e4af46
Moenul <moenul22@gmail.com>
2024-08-14 16:27:21 +0600

    CRU Task using Api and Api Routing working perfectly

---
+++ app/Http/Controllers/Api/V1/TasksController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return TaskResource::collection(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'content' => 'required|string|min:3|max:255',
+        ]);
+
+        $task = Task::create($validate);
+
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $task = Task::findOrFail($id);
+
+        return TaskResource::make($task);
+    }
+
+    // public function show(Task $task)
+    // {
+    //     return TaskResource::make($task);
+    // }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        
+        $task = Task::findOrFail($id);
+
+        $validate = $request->validate([
+            'content' => 'required|string|min:3|max:255',
+        ]);
+        
+        $task->update($validate);
+
+        return TaskResource::make($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
